62131ed822d83e85a8edd4bae61f470781636688
WilliamWillyRodriguez <33875125+WilliamWillyRodriguez@users.noreply.github.com>
2018-08-04 13:16:54 -0400

    Add files via upload

---
+++ Exercise4/PrintingChoicesEscapes.java
@@ -0,0 +1,23 @@
+public class PrintingChoicesEscapes {
+	public static void main( String[] args ) {
+		System.out.print( "Alpha\n" );
+		System.out.print( "Bravo\n" );
+		
+		System.out.print( "Charlie\n" );
+		System.out.print( "Delta\n" );
+		System.out.print( "\n" );
+		
+		System.out.print( "Echo" );
+		System.out.print( "Foxtrot" );
+		
+		System.out.print ( "Golf\n" );
+		System.out.print( "Hotel" );
+		System.out.print( "\n" );
+		System.out.print( "India\n" );
+		
+		System.out.print( "\n" );
+		System.out.print( "This" + " " + "is" + " " + "a" + " test.\n" );
+	
+		System.out.println( "I" + " " + "am" + " " + "learning" + " " + "Java" + " " + "the" + " " + "Hard" + " " + "Way!" );
+	}
+}
