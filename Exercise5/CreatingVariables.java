100fbb318f54920e9d964d813578aa9d84000f6a
WilliamWillyRodriguez <33875125+WilliamWillyRodriguez@users.noreply.github.com>
2018-08-04 13:17:14 -0400

    Add files via upload

---
+++ Exercise5/CreatingVariables.java
@@ -0,0 +1,39 @@
+public class CreatingVariables {
+	public static void main( String[] args ) {
+		int x, y, age;
+		double seconds, e, checking;
+		String firstName, lastName, title;
+		
+		x = 10;
+		y = 400;
+		age = 39;
+		
+		seconds = 4.71;
+		e = 2.71828182845904523536;
+		checking = 1.89;
+		
+		firstName = "Graham";
+		lastName = "Mitchell";
+		title = "Mr.";
+
+		//My Four Variables
+        int chapters = 16;
+        double chaptersDone = 4.5;
+        String teachersFirstName = "Peter";
+        String teachersLastName = "Melton";
+		
+		System.out.println( "The variable x contains " + x );
+		System.out.println( "The value " + y + " is stored in the variable y." );
+		System.out.println( "The experiment took " + seconds + " seconds." );
+		System.out.println( "A favorite irrational # is Euler's number: " + e );
+		System.out.println( "Hopefully you have more than $" + checking + "!" );
+		System.out.println( "My name's " + title + " " + firstName + lastName );
+		System.out.println( " " );
+		System.out.println( chapters );
+		System.out.println( chaptersDone );
+		System.out.println( teachersFirstName );
+		System.out.println( teachersLastName );
+		
+		//Finished.
+	}
+}
